295683327c369f6a8849a68f8ee2560453e64883
Adesiyan <tayonetu80@gmail.com>
2018-11-09 14:57:08 -0800

    updated blog app

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&1uh2j$acyous68s9hpioy%thff9)pxbzeb4cypd_n5^o-l_tn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
